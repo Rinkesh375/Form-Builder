6a7ea61d4dfccd207c8c71a562335b7031af8308
Rinkesh375 <rinkeshujjwal16@gmail.com>
2025-11-30 17:07:33 +0530

    fix env error of common js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 import app from "./app.js";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Backend server running successfully.`);
+  console.log(`Backend server running successfully on port ${PORT}`);
 });
